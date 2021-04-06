f8974474466cc62f78ff3839cf2a10aaebf1015f
reid.nathaniel <reid.nathaniel@olsohoio.org>
2021-04-06 12:51:16 -0400

    howdy dawg

--- mainThreading.py
+++ mainThreading.py
@@ -1,13 +1,28 @@
-from otherstudentThreading import toggle_gpio, check_temp
+
 import threading
+
+import RPi.GPIO as GPIO
+GPIO.setwarnings(False)
+
+from DoorOpener.KeypadPasswordCode import accept_code
+from otherstudentThreading import check_temp
+from DistanceLight.UltrasonicRanging import distance_light
+from austin.SenseLED import loop
 
 print('Starting...')
 
+GPIO.setmode(GPIO.BOARD)
+
+
+# keypad_worker = threading.Thread(target=accept_code)
+# keypad_worker.start()
+
 check_temp_worker = threading.Thread(target=check_temp)
 check_temp_worker.start()
+#
+distance_light_worker = threading.Thread(target=distance_light)
+distance_light_worker.start()
 
-other_worker = threading.Thread(target=toggle_gpio)
-other_worker.start()
-
-
-
+austin_worker = threading.Thread(target=loop)
+austin_worker.start()
+#
--- otherstudentThreading.py
+++ otherstudentThreading.py
@@ -9,39 +9,41 @@ import RPi.GPIO as GPIO
 import time
 import Freenove_DHT as DHT
 
-DHTPin = 11     #define the pin of DHT11
-GPIO.setmode(GPIO.BOARD)
-GPIO.setup(18, GPIO.OUT)
-GPIO.output(18, False)
-GPIO.setup(16, GPIO.OUT)
-GPIO.output(16,False)
-
-
-def toggle_gpio():
-        while True:
-            GPIO.output(18, True)
-            print("18 on!")
-            time.sleep(2)
-            GPIO.output(18, False)
-            print("18 off!")
-            time.sleep(2)
-
 
 def check_temp():
-        while True:
-            dht = DHT.DHT(DHTPin)   #create a DHT class object
-            for i in range(0,15):
-                chk = dht.readDHT11()   #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-                if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
-                    print("DHT11,OK!")
-                    break
-                else:
-                    print("Bad reading, rechecking")
-            if dht.temperature > 23:
-                GPIO.output(16,True)
+    DHTPin = 24  # define the pin of DHT11
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(26, GPIO.OUT)
+    GPIO.setup(19, GPIO.OUT)
+    GPIO.output(26, False)
+    GPIO.output(19, False)
+
+    while True:
+        dht = DHT.DHT(DHTPin)   #create a DHT class object
+        for i in range(0,15):
+            chk = dht.readDHT11()   #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+            if (chk is dht.DHTLIB_OK):      #read DHT11 and get a return value. Then determine whether data read is normal according to the return value.
+                print("DHT11,OK!")
+                break
             else:
-                GPIO.output(16,False)
-            time.sleep(0.1)
-            fahrenheit = (dht.temperature * 1.8) + 32
-            print("Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature))
-            print(f"Humidity : {dht.humidity} \t Temperature : {fahrenheit} \n")
+                print("Bad reading, rechecking")
+        if dht.temperature > 23:
+            GPIO.output(26,True)
+            GPIO.output(19, False)
+            print("Defreezing Initiated")
+
+        else:
+            GPIO.output(26,False)
+            GPIO.output(19, True)
+            print("Heating Initiated")
+
+
+        time.sleep(0.1)
+        fahrenheit = (dht.temperature * 1.8) + 32
+        print("Humidity : %.2f, \t Temperature : %.2f \n"%(dht.humidity,dht.temperature))
+        print(f"Humidity : {dht.humidity} \t Temperature : {fahrenheit} \n")
+
+
+
+if __name__ == "__main__":
+    check_temp()
